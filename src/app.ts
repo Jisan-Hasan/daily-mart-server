a1f4ab24cc2462243e86bc655ad209dd0e6ac8e2
Jisan-Hasan <jisan.jh5@gmail.com>
2023-11-12 14:19:22 +0600

    Project setup done with error handler , shared files & middleware

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import express, { Application, NextFunction, Request, Response } from 'express';
 
 import cookieParser from 'cookie-parser';
 import httpStatus from 'http-status';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import router from './app/routes';
 
 const app: Application = express();
 
@@ -12,11 +14,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //handle api
+app.use('/api/v1', router);
 
 // test api
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Hello from daily mart server!');
 });
+
+// global error handler
+app.use(globalErrorHandler);
 
 //handle not found
 app.use((req: Request, res: Response, next: NextFunction) => {
